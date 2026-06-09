b5b2b6d979edc0cbacd80286748a9adc84bbab82
Walid <walid_loudin@hotmail.com>
2026-06-08 21:47:55 -0400

    Deploying my website

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-8xl font-bold">Walid Loudin - Hey Welcome to my personal website</h1>
+      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
+        Click me!
+      </button>
+
+    </div>
+  );
+}
